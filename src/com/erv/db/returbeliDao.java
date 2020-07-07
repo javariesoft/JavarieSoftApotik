d79c20ba20686e9b4a5cb4ec323d95c2c2716cb3
USER <USER@DESKTOP-6D5F2J3.mshome.net>
2020-07-07 16:36:33 +0700

    Merge origin/master
    Conflicts:
    	src/javariesoft/faktur.json

--- src/com/erv/db/returbeliDao.java
+++ src/com/erv/db/returbeliDao.java
@@ -129,8 +129,11 @@ public class returbeliDao {
 //                + "where substring(kodereturbeli,4,2)='" + Util.getbln(tgl) + "' "
 //                + "and substring(kodereturbeli,6,2)='" + Util.getthn(tgl).substring(2, 4) + "'";
         
+        //String sql = "select max(right(kodereturbeli,4)) from returbeli "
+        //        + "where substring(kodereturbeli,4,2)='" + Util.getthn(tgl).substring(2, 4) + "'";
         String sql = "select max(right(kodereturbeli,4)) from returbeli "
-                + "where substring(kodereturbeli,4,2)='" + Util.getthn(tgl).substring(2, 4) + "'";
+                + "where substring(kodereturbeli,3,3)='." + Util.getthn(tgl).substring(2, 4) + "'";
+        
         try {
             Statement stat = conn.createStatement();
 
